c77c087f6b5226bb3f86779138a08f4229f3b8e0
lrkhafez01 <102746140+lrkhafez01@users.noreply.github.com>
2023-12-20 01:29:40 +0600

    Source Code Added

---
+++ employeeManagementSystem/src/employeemanagementsystem/database.java
@@ -0,0 +1,20 @@
+package employeemanagementsystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class database {
+    
+    public static Connection connectDb(){
+        
+        try{
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/employee", "root", "");
+            return connect;
+        }catch(Exception e){e.printStackTrace();}
+        return null;
+    }
+    
+}
